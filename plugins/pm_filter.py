2016f2a99891469091296e84d9ead8999fa283d5
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-18 17:11:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1371,7 +1371,7 @@ async def advantage_spell_chok(msg):
             
     ]]
     spl = await msg.reply_photo(
-            photo="https://telegra.ph/file/0a777cb06df4f0336861b.jpg", 
+            photo="https://telegra.ph/file/79bd06157a47319dbae24.jpg", 
             caption=NON_IMG.format(mv_rqst),
             reply_markup=InlineKeyboardMarkup(btn)
     )
